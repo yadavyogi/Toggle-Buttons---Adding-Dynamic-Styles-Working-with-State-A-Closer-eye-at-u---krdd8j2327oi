e2617ec94212086619b12df61fb5dcd39eb0e50c
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2022-12-06 12:55:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import React, {useState} from 'react'
-import '../styles/App.css';
-const App = () => {
-//code here 
-  
+import React, { useState } from "react";
+import "../styles/App.css";
 
-  
+const App = () => {
+  const [state, setState] = useState(true)
+  const handleToggle = () => {
+       if(state === true) {
+            setState(false)
+       } else {
+        setState(true)
+       }
+  }
   return (
-    <div className="App">
-      <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
-      </button>
-      <br />
-      <br />
-      <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
-      </button>
+    <div id="main">
+      {state === true ? (
+        <div id="welcome-div">{<h1>Welcome to Newton School!!</h1>}</div>) : (
+        <div id="about-div">{<h1>Newton School is a neo-university providing highly
+           immersive tech learning to millions of students, to tap into new-age 
+           tech opportunities.</h1>}</div>
+        )}
+      <button id="toggle" onClick={handleToggle}>Toggle</button>
     </div>
   );
-}
-
+};
 
 export default App;
