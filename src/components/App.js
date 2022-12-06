41a86898af275c33a3563de864bf2569d9c5c207
yadavyogi <115787274+yadavyogi@users.noreply.github.com>
2022-12-06 13:02:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
-import "../styles/App.css";
-
+import '../styles/App.css';
 const App = () => {
-  const [state, setState] = useState(true)
-  const handleToggle = () => {
-       if(state === true) {
-            setState(false)
-       } else {
-        setState(true)
-       }
-  }
+  const [val, setVal] = useState(false);
+  //code here
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (e.target.id === "btn1") {
+      setVal(!val);
+    } else {
+      setVal(!val);
+    }
+  };
+
   return (
-    <div id="main">
-      {state === true ? (
-        <div id="welcome-div">{<h1>Welcome to Newton School!!</h1>}</div>) : (
-        <div id="about-div">{<h1>Newton School is a neo-university providing highly
-           immersive tech learning to millions of students, to tap into new-age 
-           tech opportunities.</h1>}</div>
-        )}
-      <button id="toggle" onClick={handleToggle}>Toggle</button>
+    <div className="App">
+      <button id="btn1" onClick={handleClick}>
+        {/* assign value for button 1 */}
+        {!val ? "OFF" : "ON"}
+      </button>
+      <br />
+      <br />
+      <button id="btn2" onClick={handleClick}>
+        {/* assign value for button 2 */}
+        {!val ? "ON" : "OFF"}
+      </button>
     </div>
   );
 };
